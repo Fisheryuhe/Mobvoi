da0a1d0c93d8b51d0ffce2d15f3b01851c29133d
chhyu <chhyu@mobvoi.com>
2017-01-07 11:04:09 +0800

    delete file

--- data-mining/src/main/java/com/mobvoi/be/usercenter/datamining/model/DistrictData.java
+++
@@ -1,9 +0,0 @@
-//Copyright 2016 Mobvoi Inc. All Rights Reserved
-//Author : chhyu@mobvoi.com(Changhe Yu)
-
-package com.mobvoi.be.usercenter.datamining.model;
-
-import java.util.ArrayList;
-
-@SuppressWarnings("serial")
-public class DistrictData extends ArrayList<String> {}
